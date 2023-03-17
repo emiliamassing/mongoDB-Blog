fc73c5763742d747270161ec71115b6c689da831
emiliamassing <emiliamassings@gmail.com>
2023-03-17 11:19:54 +0100

    🎨Improved code structure

--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const BlogpostModel = require('../models/blog-model');
 
 /* GET users listing. */
-router.get('/', async (req, res) => {
+router.get('/', async function(req, res) {
   const blogposts = await BlogpostModel.find();
   res.status(200).json(blogposts);
 });
 
-router.post('/add', async (req, res) => {
+router.post('/add', async function(req, res) {
   const blogpost = await BlogpostModel.create(req.body);
 
   res.status(201).json(blogpost);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const UserModel = require('../models/user-model');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res) {
+  const users = await UserModel.find();
+  res.status(200).json.apply(users);
+});
+
+router.post('/add', async function(req, res){
+  const user = await UserModel.create(req.body);
+  res.status(201).json(user);
 });
 
 module.exports = router;
