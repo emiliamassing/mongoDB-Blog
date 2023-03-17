25c69f9addb7743d3d5ed401aa5ba8f2d2e5aaf6
emiliamassing <emiliamassings@gmail.com>
2023-03-17 12:23:17 +0100

    ✨Added router to login

--- routes/users.js
+++ routes/users.js
@@ -13,10 +13,25 @@ router.post('/add', async function(req, res){
   let newUser = await UserModel.create({
     username: req.body.username,
     password: CryptoJs.SHA3(req.body.password).toString()
-  })
+  });
 
   console.log("New user", newUser);
   res.status(201).json(newUser);
+});
+
+router.post('/login', async function(req, res) {
+  const login = {
+    username: req.body.username,
+    password: req.body.password
+  }
+
+  let findUser = await UserModel.findOne({username: login.username});
+  if(CryptoJs.SHA3(login.password).toString() === findUser.password) {
+    res.status(201).json({username: findUser.username});
+    console.log('Login succecssful');
+  } else {
+    res.status(401).json('Incorrect username or password');
+  }
 });
 
 module.exports = router;
